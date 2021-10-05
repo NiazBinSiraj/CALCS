88fd72aabab9aa835b406bf83b3c72643802ceea
Niaz Bin Siraj <niaz9767@gmail.com>
2021-10-06 05:33:26 +0600

    newui implemented successfully

--- CALCS-Frontend/src/app-state.ts
+++ CALCS-Frontend/src/app-state.ts
@@ -2,13 +2,14 @@ import { Clerk } from "./app/models/clerk";
 export class AppState{
     public static instance = new AppState;
 
-    
+    backendURL:string = "http://127.0.0.1:8000";
+
     access_token:string = "";
     user_id:number = 0;
     clerk_id:number = 0;
-    username:string = "";
-    user_type:string = "";
-    isLoggedIn:boolean = false;
+    username:string = "USER";
+    user_type:string = "C";
+    isLoggedIn:boolean = true;
 
     clerkUser:Clerk = new Clerk();
 }
